fe1c1c75cb4d9f119090f3bdf53dcc4c4edd1c39
Emmanuel Ekenedo <54508360+Emmanuel1539@users.noreply.github.com>
2024-04-28 00:58:06 +0100

    clone changes

---
+++ scripts/getdates.js
@@ -0,0 +1,18 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const fullYear = document.querySelector("#currentyear");
+    const lastModified = document.querySelector("#lastmodified");
+
+    // use date object 
+    const today = new Date();
+
+    fullYear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+    lastModified.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat(
+        "en-US",
+        {
+            dateStyle: "full"
+        }
+    ).format(today)}</span>`;
+});
